47389b3ea08066e98229bbbe456d6b4ead1766fd
dataisland <dataisland@outlook.com>
2023-02-05 21:00:17 -0600

    Add Escape and BitCast filter

--- llvm-project/llvm/lib/Transforms/Instrumentation/OverflowDefense.cpp
+++ llvm-project/llvm/lib/Transforms/Instrumentation/OverflowDefense.cpp
@@ -2,6 +2,7 @@
 //------------===//
 
 #include "llvm/Transforms/Instrumentation/OverflowDefense.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Support/CommandLine.h"
 
 using namespace llvm;
@@ -19,6 +20,68 @@ static cl::opt<bool> ClKeepGoing("odef-keep-going",
 
 namespace {
 
+class OverflowDefense {
+public:
+  OverflowDefense(Module &M, const OverflowDefenseOptions &Options)
+      : Kernel(Options.Kernel), Recover(Options.Recover) {
+    initializeModule(M);
+  }
+
+  OverflowDefense(const OverflowDefense &&) = delete;
+  OverflowDefense &operator=(const OverflowDefense &&) = delete;
+  OverflowDefense(const OverflowDefense &) = delete;
+  OverflowDefense &operator=(const OverflowDefense &) = delete;
+
+  bool sanitizeFunction(Function &F);
+
+private:
+  void initializeModule(Module &M);
+  void collectToInstrument(Function &F);
+
+  bool filterToInstrument(Function &F, Instruction *I);
+  bool NeverEscaped(Instruction *I);
+  bool NeverEscapedInternal(Instruction *I,
+                            SmallPtrSet<Instruction *, 16> &Visited);
+  bool isShrinkBitCast(Instruction *I);
+
+  bool Kernel;
+  bool Recover;
+
+  SmallVector<GetElementPtrInst *, 16> GepToInstrument;
+  SmallVector<BitCastInst *, 16> BcToInstrument;
+
+  const DataLayout *DL;
+};
+
+bool isEscapeInstruction(Instruction *I) {
+  // TODO: Add uncommon escape instructions
+  if (isa<StoreInst>(I) || isa<LoadInst>(I) || isa<ReturnInst>(I))
+    return true;
+
+  static SmallVector<StringRef, 16> whitelist = {"llvm.prefetch."};
+
+  if (auto *CI = dyn_cast<CallInst>(I)) {
+    Function *F = CI->getCalledFunction();
+    if (F != nullptr) {
+      for (auto &name : whitelist) {
+        if (F->getName().startswith(name))
+          return false;
+      }
+
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool isUnionType(Type *Ty) {
+  if (auto *STy = dyn_cast<StructType>(Ty))
+    return STy->hasName() && STy->getName().startswith("union.");
+
+  return false;
+}
+
 template <class T> T getOptOrDefault(const cl::opt<T> &Opt, T Default) {
   return (Opt.getNumOccurrences() > 0) ? Opt : Default;
 }
@@ -30,10 +93,95 @@ OverflowDefenseOptions::OverflowDefenseOptions(bool Kernel, bool Recover)
 
 PreservedAnalyses OverflowDefensePass::run(Function &F,
                                            FunctionAnalysisManager &FAM) {
+  OverflowDefense Odef(*F.getParent(), Options);
+  if (Odef.sanitizeFunction(F))
+    return PreservedAnalyses::none();
   return PreservedAnalyses::all();
 }
 
 PreservedAnalyses ModuleOverflowDefensePass::run(Module &M,
                                                  ModuleAnalysisManager &AM) {
   return PreservedAnalyses::all();
+}
+
+void OverflowDefense::initializeModule(Module &M) { DL = &M.getDataLayout(); }
+
+bool OverflowDefense::sanitizeFunction(Function &F) {
+  collectToInstrument(F);
+
+  return false;
+}
+
+void OverflowDefense::collectToInstrument(Function &F) {
+  // TODO: collect glibc function call
+  for (auto &BB : F) {
+    for (auto &I : BB) {
+      if (auto *Gep = dyn_cast<GetElementPtrInst>(&I)) {
+        if (!filterToInstrument(F, Gep))
+          GepToInstrument.push_back(Gep);
+      } else if (auto *Bc = dyn_cast<BitCastInst>(&I)) {
+        if (!filterToInstrument(F, Bc))
+          BcToInstrument.push_back(Bc);
+      }
+    }
+  }
+}
+
+bool OverflowDefense::filterToInstrument(Function &F, Instruction *I) {
+  assert(I->getType()->isPointerTy());
+
+  if (NeverEscaped(I))
+    return true;
+
+  if (isShrinkBitCast(I))
+    return true;
+
+  return false;
+}
+
+bool OverflowDefense::NeverEscaped(Instruction *I) {
+  SmallPtrSet<Instruction *, 16> Visited;
+  return NeverEscapedInternal(I, Visited);
+}
+
+bool OverflowDefense::NeverEscapedInternal(
+    Instruction *I, SmallPtrSet<Instruction *, 16> &Visited) {
+  if (Visited.count(I))
+    return true;
+
+  Visited.insert(I);
+
+  for (auto *U : I->users()) {
+    if (auto *UI = dyn_cast<Instruction>(U)) {
+      if (isEscapeInstruction(UI))
+        return false;
+
+      if (!NeverEscapedInternal(UI, Visited))
+        return false;
+    } else {
+      // TODO: handle non-instruction user
+    }
+  }
+
+  return true;
+}
+
+bool OverflowDefense::isShrinkBitCast(Instruction *I) {
+  if (auto *BC = dyn_cast<BitCastInst>(I)) {
+    if (!BC->getSrcTy()->isPointerTy() || !BC->getDestTy()->isPointerTy())
+      return false;
+
+    Type *srcTy = BC->getSrcTy()->getPointerElementType();
+    Type *dstTy = BC->getDestTy()->getPointerElementType();
+
+    if (!srcTy->isSized() || !dstTy->isSized())
+      return false;
+
+    TypeSize srcSize = DL->getTypeStoreSize(srcTy);
+    TypeSize dstSize = DL->getTypeStoreSize(dstTy);
+
+    return dstSize <= srcSize;
+  }
+
+  return false;
 }
