9149c55bb22271da161f7b021d9c58c953b1403f
dataisland <dataisland@outlook.com>
2023-02-23 22:01:41 -0600

    Update analysis method

--- tools/analysis.py
+++ tools/analysis.py
@@ -4,11 +4,7 @@
 import os
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: %s <file>" % sys.argv[0])
-    sys.exit(1)
-
-else:
+def analysis_compile_log():
   filepath = os.path.realpath(sys.argv[1])
   print("Starting analysis of %s" % filepath)
 
@@ -32,7 +28,62 @@ else:
       })
       
   fn.sort(key=lambda x: x['runtime'] + x['cluster'] * 1.1 + x['builtin'] * 0.1, reverse=True)
-  for i, f in enumerate(fn):
-    if f['runtime'] + f['cluster'] <= 2:
-      continue
-    print(f"{i} {f['name']}: {f['runtime']} {f['cluster']} {f['builtin']}")
+
+  return fn
+
+def analysis_run_log():
+  filepath = os.path.realpath(sys.argv[2])
+  print("Starting analysis of %s" % filepath)
+
+  with open(filepath, "r") as f:
+    lines = f.read().strip().splitlines()
+
+  cnt = []
+  for line in lines:
+    fn, count = line.split(':')
+    cnt.append((fn, int(count)))
+
+  return cnt
+
+if len(sys.argv) < 2:
+    print("Usage: %s <file>" % sys.argv[0])
+    sys.exit(1)
+
+else:
+  fn = analysis_compile_log()
+
+  if len(sys.argv) == 2:
+    for i, f in enumerate(fn):
+      if f['runtime'] + f['cluster'] <= 2:
+        continue
+      print(f"{i} {f['name']}: {f['runtime']} {f['cluster']} {f['builtin']}")
+  else:
+    cnt = analysis_run_log()
+
+    data = []
+    for c in cnt:  
+      for f in fn:
+        if f['name'] == c[0]:
+          data.append(
+            {
+              'name': f['name'],
+              'runtime': f['runtime'],
+              'cluster': f['cluster'],
+              'builtin': f['builtin'],
+              'count': c[1],
+            }
+          )
+          break
+
+    def weight(x):
+      return x['count'] * (x['runtime'] + x['cluster'] * 1.1 + x['builtin'] * 0.1)
+
+    data.sort(key=weight, reverse=True)
+    s = sum(weight(x) for x in data)
+
+    v = 0
+    for i, x in enumerate(data):
+      v += weight(x) / s * 100
+      print(f"{i} {round(weight(x) / s * 100, 2)}% {x['name']}: {x['runtime']} {x['cluster']} {x['builtin']} {x['count']}")
+      if v > 99:
+        break
