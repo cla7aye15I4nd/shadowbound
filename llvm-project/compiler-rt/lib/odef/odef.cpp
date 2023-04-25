67ffbb705e52af5c560a5cbe2924854484f93f13
dataisland <dataisland@outlook.com>
2023-04-24 21:04:31 -0500

    Add small chunk optimization runtime support

--- llvm-project/compiler-rt/lib/odef/odef.cpp
+++ llvm-project/compiler-rt/lib/odef/odef.cpp
@@ -10,22 +10,53 @@
 #include "sanitizer_common/sanitizer_libc.h"
 #include "sanitizer_common/sanitizer_linux.h"
 
+extern "C" SANITIZER_WEAK_ATTRIBUTE const int __odef_only_small_alloc_opt;
+
 namespace __odef {
 
 void SetShadow(const void *ptr, uptr size) {
+  // Layout of the shadow memory:
+  //  |BACK|FRON|....|BACK|FRON|BACK|FRON|....|BACK|FRON|
+  // Layout Example for size = 0x100:
+  // if __odef_only_small_alloc_opt:
+  //    |0x000|0x100|0x008|0x0f8|0x010|0x0f0|....|0x0f0|0x010|0x0f8|0x008|0x100|0x000|
+  // else
+  //    |0x00|0x20|0x01|0x1f|0x02|0x1e|....|0x1e|0x02|0x1f|0x01|0x20|0x00|
+
   u32 *shadow_beg = (u32 *)MEM_TO_SHADOW(ptr);
   u32 *shadow_end = shadow_beg + size / sizeof(u32);
 
-  u32 a = 0;
-  u32 b = size / sizeof(uptr);
+  if (__odef_only_small_alloc_opt) {
+    if (size > u32(-1)) {
+      Report("ERROR: __odef_only_small_alloc_opt is enabled, but the size "
+             "of the allocation is too big: %zu\n",
+             size);
+      Die();
+    }
+
+    u32 a = 0;
+    u32 b = size;
 
 #ifdef __clang__
 #pragma unroll
 #endif
-  while (shadow_beg < shadow_end) {
-    *(shadow_beg + 0) = b--;
-    *(shadow_beg + 1) = a++;
-    shadow_beg += 2;
+    while (shadow_beg < shadow_end) {
+      *(shadow_beg + 0) = b -= sizeof(u64);
+      *(shadow_beg + 1) = a += sizeof(u64);
+      shadow_beg += 2;
+    }
+  } else {
+    u32 a = 0;
+    u32 b = size / sizeof(u32);
+
+#ifdef __clang__
+#pragma unroll
+#endif
+    while (shadow_beg < shadow_end) {
+      *(shadow_beg + 0) = b--;
+      *(shadow_beg + 1) = a++;
+      shadow_beg += 2;
+    }
   }
 }
 
@@ -61,9 +92,7 @@ void __odef_init() {
   odef_inited = true;
 }
 
-void __odef_report() {
-  Report(" Overflow detected\n");
-}
+void __odef_report() { Report(" Overflow detected\n"); }
 
 void __odef_abort() {
   __odef_report();
