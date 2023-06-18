24ed791d39645961523eedeb8f35827ae1f2187f
dataisland <dataisland@outlook.com>
2023-06-18 05:04:20 -0500

    Finish the stack optimization

--- analyzer/src/harness/pattern.cpp
+++ analyzer/src/harness/pattern.cpp
@@ -1,8 +1,8 @@
 #include "pattern.h"
 
 #include "llvm/Support/Debug.h"
-#include "llvm/Support/JSON.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/JSON.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <algorithm>
@@ -12,8 +12,52 @@
 using namespace std;
 using namespace llvm;
 
-static bool alwaysNonHeap(Function &F, unsigned int ArgNo,
+static bool isHeapAddress(Value *V) {
+  set<Value *> Visited;
+  vector<Value *> Worklist;
+
+  Worklist.push_back(V);
+  while (!Worklist.empty()) {
+    Value *V = Worklist.back();
+    Worklist.pop_back();
+
+    if (Visited.find(V) != Visited.end())
+      continue;
+    Visited.insert(V);
+
+    if (auto *GEP = dyn_cast<GetElementPtrInst>(V))
+      Worklist.push_back(GEP->getPointerOperand());
+    else if (auto *BC = dyn_cast<BitCastInst>(V))
+      Worklist.push_back(BC->getOperand(0));
+    else if (auto *PHI = dyn_cast<PHINode>(V))
+      for (unsigned int i = 0; i < PHI->getNumIncomingValues(); i++)
+        Worklist.push_back(PHI->getIncomingValue(i));
+    else if (!(isa<GlobalValue>(V) || isa<AllocaInst>(V) || isa<Constant>(V)))
+      return true;
+  }
+
+  return true;
+}
+
+static bool alwaysNonHeap(Function &FO, unsigned int ArgNo,
                           vector<Module *> &Modules) {
+  for (auto *M : Modules) {
+    for (auto &F : *M) {
+      if (F.isDeclaration())
+        continue;
+      for (auto &BB : F) {
+        for (auto &I : BB) {
+          if (auto *CI = dyn_cast<CallBase>(&I)) {
+            if (CI->getCalledFunction() == &FO) {
+              Value *Arg = CI->getArgOperand(ArgNo);
+              if (isHeapAddress(Arg))
+                return false;
+            }
+          }
+        }
+      }
+    }
+  }
   return true;
 }
 
