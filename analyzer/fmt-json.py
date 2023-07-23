655fff3bb20cdff63c66655a850ed5d3ef721eaf
dataisland <dataisland@outlook.com>
2023-07-22 22:42:46 -0500

    Update formatter

--- analyzer/fmt-json.py
+++ analyzer/fmt-json.py
@@ -8,6 +8,7 @@ if len(sys.argv) < 2:
 input_file = sys.argv[1]
 with open(input_file, 'r') as f:
     data = json.load(f)
+    data = sorted(data, key=lambda x : json.dumps(x, sort_keys=True))
 
 with open(input_file, 'w') as f:
     json.dump(data, f, indent=2, sort_keys=True)
